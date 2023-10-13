8f8b640475d11cd4218677882fd2099defc8f2e5
Dan Murfitt <dan@dmurfitt.co.uk>
2023-10-13 10:27:56 +0100

    Add Productivity profile content

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import GetHero from './components/ui/herobanner/hero'
 import GetInput from './components/ui/input/input'
 import GetTextArea from './components/ui/textarea/textarea'
 import GetMandatoryBonusDuties from './components/logic/selectpopulators/mbd'
+import GetInputNumber from './components/ui/input/inputNumber'
 
 function App() {
     return (
@@ -47,6 +48,99 @@ function App() {
             </div>
             <div className="column is-6">
               <GetTextArea id="servicegroup" placeholder="Service group" helpText="" label="Service group" rows={13}></GetTextArea>
+            </div>
+          </div>
+        </section>
+
+        <section className="section container" id="section-PP">
+          <h1 className="container title">Productivity Profile</h1>
+          <div className="columns is-multiline">
+            <div className="column">
+              <div className="columns is-mobile">
+                <div className="column is-6">
+                  <div className="columns is-multiline">
+                    <div className="column is-12">
+                      <GetInputNumber id="brains" placeholder="Brains" helpText="" label="Brains" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="alpha-complex" placeholder="Alpha Complex" helpText="" label="Alpha Complex" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="bureaucracy" placeholder="Bureaucracy" helpText="" label="Bureaucracy" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="psychology" placeholder="Psychology" helpText="" label="Psychology" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="science" placeholder="Science" helpText="" label="Science" maximum={5} minimum={-5}></GetInputNumber>
+                      <div className='is-hidden-desktop pb-6'>
+                </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="column is-6">
+                  <div className="columns is-multiline">
+                    <div className="column is-12">
+                      <GetInputNumber id="chutzpah" placeholder="Chutzpah" helpText="" label="Chutzpah" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="bluff" placeholder="Bluff" helpText="" label="Bluff" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      {" "}
+                      <GetInputNumber id="charm" placeholder="Charm" helpText="" label="Charm" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="intimidate" placeholder="Intimidate" helpText="" label="Intimidate" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="stealth" placeholder="Stealth" helpText="" label="Stealth" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="columns is-mobile">
+                <div className="column is-6">
+                  <div className="columns is-multiline">
+                    <div className="column is-12">
+                      <GetInputNumber id="mechanics" placeholder="Mechanics" helpText="" label="Mechanics" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="demolition" placeholder="Demolition" helpText="" label="Demolition" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="engineer" placeholder="Engineer" helpText="" label="Engineer" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="operate" placeholder="Operate" helpText="" label="Operate" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="program" placeholder="Program" helpText="" label="Program" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                  </div>
+                </div>
+                <div className="column is-6">
+                  <div className="columns is-multiline">
+                    <div className="column is-12">
+                      <GetInputNumber id="violence" placeholder="Violence" helpText="" label="Violence" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="athletics" placeholder="Athletics" helpText="" label="Athletics" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="guns" placeholder="Guns" helpText="" label="Guns" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="melee" placeholder="Melee" helpText="" label="Melee" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                    <div className="column is-6">
+                      <GetInputNumber id="throw" placeholder="Throw" helpText="" label="Throw" maximum={5} minimum={-5}></GetInputNumber>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
---
+++ src/components/ui/input/inputNumber.test.tsx
@@ -0,0 +1,87 @@
+import GetInputNumber, {inputNumProps} from './inputNumber';    
+import {render, screen, fireEvent} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+
+
+const inputObj:inputNumProps ={
+    id: "testingId",
+    placeholder: 'placeholdertext',
+    helpText: 'helptext',
+    label: 'labeltext',
+    maximum:5,
+    minimum:-5
+}
+
+const emptyHelpInputObj:inputNumProps ={
+    id: "testingId",
+    placeholder: 'placeholdertext',
+    helpText: '',
+    label: 'labeltext',
+    maximum:5,
+    minimum:-5
+}
+
+
+describe("UI input number", function () {
+  test("is rendered", () => {
+    render(<GetInputNumber id={inputObj.id} placeholder={inputObj.placeholder} helpText={inputObj.helpText} label={inputObj.label} maximum={inputObj.maximum} minimum={inputObj.minimum} />);
+    const inputItem = screen.getByRole("input");
+    expect(inputItem).toBeInTheDocument();
+  });
+
+  test("has correct label", () => {
+    render(<GetInputNumber id={inputObj.id} placeholder={inputObj.placeholder} helpText={inputObj.helpText} label={inputObj.label} maximum={inputObj.maximum} minimum={inputObj.minimum} />);
+    const inputItem = screen.getByLabelText(inputObj.label);
+    expect(inputItem).toBeInTheDocument();
+  });
+
+  test("shows help when exists", () => {
+    render(<GetInputNumber id={inputObj.id} placeholder={inputObj.placeholder} helpText={inputObj.helpText} label={inputObj.label} maximum={inputObj.maximum} minimum={inputObj.minimum} />);
+    const inputItem = screen.getByText(inputObj.helpText);
+    expect(inputItem).toHaveClass("help");
+    expect(inputItem).toBeInTheDocument();
+  });
+
+  test("hides help when not exists", () => {
+    render(<GetInputNumber id={emptyHelpInputObj.id} placeholder={emptyHelpInputObj.placeholder} helpText={emptyHelpInputObj.helpText} label={emptyHelpInputObj.label} maximum={inputObj.maximum} minimum={inputObj.minimum} />);
+    const inputItem = screen.getByRole("input");
+    expect(inputItem).not.toContain("help");
+  });
+
+  test("maximum value is adhered to", async () => {
+    const user = userEvent.setup();
+    render(<GetInputNumber id={emptyHelpInputObj.id} placeholder={emptyHelpInputObj.placeholder} helpText={emptyHelpInputObj.helpText} label={emptyHelpInputObj.label} maximum={inputObj.maximum} minimum={inputObj.minimum} />);
+    const input = screen.getByPlaceholderText(emptyHelpInputObj.placeholder);
+    await user.type(input, "5");
+    expect(input).toHaveValue(5);
+    expect(input).toBeValid;
+  });
+
+  test("minimum value is adhered to", async () => {
+    const user = userEvent.setup();
+    render(<GetInputNumber id={emptyHelpInputObj.id} placeholder={emptyHelpInputObj.placeholder} helpText={emptyHelpInputObj.helpText} label={emptyHelpInputObj.label} maximum={inputObj.maximum} minimum={inputObj.minimum} />);
+    const input = screen.getByPlaceholderText(emptyHelpInputObj.placeholder);
+    await user.type(input, "-5");
+    expect(input).toHaveValue(-5);
+    expect(input).toBeValid;
+  });
+
+  test("exceeding minimum value is invalid", async () => {
+    const user = userEvent.setup();
+    render(<GetInputNumber id={emptyHelpInputObj.id} placeholder={emptyHelpInputObj.placeholder} helpText={emptyHelpInputObj.helpText} label={emptyHelpInputObj.label} maximum={inputObj.maximum} minimum={inputObj.minimum} />);
+    const input = screen.getByPlaceholderText(emptyHelpInputObj.placeholder);
+    await user.type(input, "-6");
+    expect(input).toHaveValue(-6);
+    expect(input).not.toBeValid;
+  });
+
+  test("exceeding maximum value is invalid", async () => {
+    const user = userEvent.setup();
+    render(<GetInputNumber id={emptyHelpInputObj.id} placeholder={emptyHelpInputObj.placeholder} helpText={emptyHelpInputObj.helpText} label={emptyHelpInputObj.label} maximum={inputObj.maximum} minimum={inputObj.minimum} />);
+    const input = screen.getByPlaceholderText(emptyHelpInputObj.placeholder);
+    await user.type(input, "6");
+    expect(input).toHaveValue(6);
+    expect(input).not.toBeValid;
+  });
+});
